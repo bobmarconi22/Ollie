69b7d01945dce2fbfa2b2a343df58f8708294a22
Bob <bobmarconi22@gmail.com>
2024-06-26 14:47:50 -0400

    about page links changed

--- react-vite/src/components/Navigation/AboutButton.jsx
+++ react-vite/src/components/Navigation/AboutButton.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, useRef } from "react";
 
-
 function AboutButton() {
   const [showMenu, setShowMenu] = useState(false);
   const ulRef = useRef();
@@ -27,78 +26,77 @@ function AboutButton() {
   return (
     <>
       <button onClick={toggleMenu} id={"user-menu"}>
-        <img src="/paw.png" alt="" style={{width: '40px', cursor: 'pointer'}}/>
+        <img
+          src="/paw.png"
+          alt=""
+          style={{ width: "40px", cursor: "pointer" }}
+        />
       </button>
-            <div className={showMenu ? "about-dropdown" : "about-dropdown-hidden"}>
-            <div
-      style={{
-        display: "flex",
-        flexWrap: 'wrap',
-        textAlign: 'center',
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      <h1
-        className="form-title"
-        style={{ position: "absolute", top: "10px", left: 0, right: 0 }}
-      >
-        About
-      </h1>
-      <img
-        src="/me.png"
-        className="pfp"
-        style={{
-          margin: "0 auto",
-          marginTop: "100px",
-        }}
-        alt=""
-      />
-      <p style={{ textAlign: "center" }}>Bob Marconi -- Software Engineer</p>
-      <p style={{ textAlign: "center" }}>
-        Follow or see my other projects below!
-      </p>
-      <div id="links-div">
-        <a
-          href="https://github.com/bobmarconi22"
-          target="_blank"
-          rel="noreferrer"
+      <div className={showMenu ? "about-dropdown" : "about-dropdown-hidden"}>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            textAlign: "center",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
         >
+          <h1
+            className="form-title"
+            style={{ position: "absolute", top: "10px", left: 0, right: 0 }}
+          >
+            About
+          </h1>
           <img
-            src="https://cdn0.iconfinder.com/data/icons/eon-social-media-contact-info-2/32/github_coding_dev_developer-512.png"
-            className="link-img"
-            alt="github"
+            src="/me.png"
+            className="pfp"
+            style={{
+              margin: "0 auto",
+              marginTop: "100px",
+            }}
+            alt=""
           />
-        </a>
-        <a
-          href="https://www.facebook.com/Bobmarconi22/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src="https://cdn3.iconfinder.com/data/icons/remixicon-logos/24/facebook-circle-line-512.png"
-            className="link-img"
-            alt="facebook"
-          />
-        </a>
-        <a href="mailto:bobmarconi22@gmail.com">
-          <img
-            src="https://cdn1.iconfinder.com/data/icons/ionicons-sharp-vol-1/512/at-sharp-512.png"
-            className="link-img"
-            alt="email"
-          />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/bob-marconi-3656932a9/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src="/linkdin.png" className="link-img" alt="linkdin" />
-        </a>
+          <p style={{ textAlign: "center" }}>
+            Bob Marconi -- Software Engineer
+          </p>
+          <p style={{ textAlign: "center" }}>
+            Follow or see my other projects below!
+          </p>
+          <div id="links-div">
+            <a
+              href="https://www.bobmarconi.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src="https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/globe-256.png"
+                className="link-img"
+                alt="bobmarconi.com"
+              />
+            </a>
+            <a
+              href="https://github.com/bobmarconi22"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src="https://cdn0.iconfinder.com/data/icons/eon-social-media-contact-info-2/32/github_coding_dev_developer-512.png"
+                className="link-img"
+                alt="github"
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/bobmarconi22/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src="/linkdin.png" className="link-img" alt="linkdin" />
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
-            </div>
     </>
   );
 }
